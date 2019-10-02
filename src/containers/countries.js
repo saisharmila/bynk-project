316bdf0d82c688d672fdc4ebcc7eb0e5d1aa234d
saisharmila <saisharmila1112@gmail.com>
2019-10-02 23:36:54 +0200

    added support to store field data in localstorage

--- src/containers/countries.js
+++ src/containers/countries.js
@@ -5,6 +5,16 @@ import {fetchCountries} from "../actions/actions"
 
 class Countries extends React.Component{
 
+    constructor(props){
+        super(props)
+
+        this.state = {
+            selectedCounty : localStorage.getItem("country") ? localStorage.getItem("country") : ""
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+    }
+
     componentDidMount(){
         this.props.fetchCountries()
     }
@@ -15,11 +25,18 @@ class Countries extends React.Component{
         })
     }
 
+    handleChange(e){
+        this.setState({
+            selectedCounty : e.target.value
+        })
+        window.localStorage.setItem("country", e.target.value)
+    }
+
     render(){
         if(this.props.countries){
             return(
                 <div className="input-field col s12 m6">
-                    <select id="countries" className="icons">
+                    <select value={this.state.selectedCounty} onChange={this.handleChange} id="countries" className="icons">
                         {this.countriesMapper(this.props.countries)}
                     </select>
                 </div>
--- src/containers/ssn.js
+++ src/containers/ssn.js
@@ -9,12 +9,24 @@ class SwedishSSN extends React.Component{
     constructor(props){
         super(props)
 
+        this.state = {
+            ssn : window.localStorage.getItem("ssn") ? window.localStorage.getItem("ssn") : ""
+        }
+
+        this.handleChange = this.handleChange.bind(this)
         this.checkValidity = this.checkValidity.bind(this)
 
     }
 
     checkValidity(event){
         this.props.validateSSN(event.target.value)
+    }
+
+    handleChange(e){
+        this.setState({
+            ssn : e.target.value
+        })
+        window.localStorage.setItem("ssn", e.target.value)
     }
 
     render(){
@@ -24,7 +36,7 @@ class SwedishSSN extends React.Component{
         <div>
             <div className="input-field col s6">
             <i className="material-icons prefix">account_circle</i>
-            <input id="ssn" onBlur={this.checkValidity} type="text" className="validate" />
+            <input onChange={this.handleChange} value={this.state.ssn} id="ssn" onBlur={this.checkValidity} type="text" className="validate" />
             <label for="ssn">Social security number</label>
             <span  style={{visibility : "hidden"}} id="helper-text-ssn" className="helper-text">Invalid Swedish security number!</span>
         </div>
