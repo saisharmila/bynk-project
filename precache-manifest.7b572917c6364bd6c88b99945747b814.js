abcc16aa6c1b85a1dea2fca940b82b41ad3deeb2
saisharmila <saisharmila1112@gmail.com>
2019-10-03 13:51:25 +0200

    Updates

---
+++ precache-manifest.7b572917c6364bd6c88b99945747b814.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6145bcea9e925624c12419587471780e",
+    "url": "/bynk-preview/build/index.html"
+  },
+  {
+    "revision": "d783d6334979c5f15134",
+    "url": "/bynk-preview/build/static/css/main.40bae443.chunk.css"
+  },
+  {
+    "revision": "b642bc9d66045c9c9506",
+    "url": "/bynk-preview/build/static/js/2.0e3d11a5.chunk.js"
+  },
+  {
+    "revision": "d783d6334979c5f15134",
+    "url": "/bynk-preview/build/static/js/main.88bf23c8.chunk.js"
+  },
+  {
+    "revision": "f663e99f400e979c83b4",
+    "url": "/bynk-preview/build/static/js/runtime-main.4e4a236b.js"
+  }
+]);
